0ec0d3591953be752dc34bb8986a316a4f9a818a
TheHajuenter <esjeruk517@gmail.com>
2025-11-16 20:16:42 +0700

    feat: add Firebase auth middleware to season routes

--- server/app/routes/season_route.py
+++ server/app/routes/season_route.py
@@ -4,9 +4,12 @@ from app.controllers.season_controller import (
     stop_season_controller,
     get_current_season_controller,
 )
+from app.middleware.auth_middleware import firebase_required
 
 season_bp = Blueprint("season_bp", __name__, url_prefix="/api/season")
 
-season_bp.route("/start", methods=["POST"])(start_season_controller)
-season_bp.route("/stop", methods=["POST"])(stop_season_controller)
-season_bp.route("/current", methods=["GET"])(get_current_season_controller)
+season_bp.route("/start", methods=["POST"])(firebase_required(start_season_controller))
+season_bp.route("/stop", methods=["POST"])(firebase_required(stop_season_controller))
+season_bp.route("/current", methods=["GET"])(
+    firebase_required(get_current_season_controller)
+)
